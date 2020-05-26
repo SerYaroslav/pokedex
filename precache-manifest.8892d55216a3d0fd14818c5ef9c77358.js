ff99aeccf5df314fb45e71a2145fdd43571f0123
Yaroslav Serediuk <seredyukua@gmail.com>
2020-05-27 00:42:36 +0300

    Updates

---
+++ precache-manifest.8892d55216a3d0fd14818c5ef9c77358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e680b04258ea9f3e619abb4980305948",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "6f8b3aa5c6a8d6338ded",
+    "url": "/pokedex/static/css/main.640a7e64.chunk.css"
+  },
+  {
+    "revision": "a1fc106c90f2299743e5",
+    "url": "/pokedex/static/js/2.b8edec15.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/pokedex/static/js/2.b8edec15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8b3aa5c6a8d6338ded",
+    "url": "/pokedex/static/js/main.6127ad1d.chunk.js"
+  },
+  {
+    "revision": "f8f50f0bb2638f4ef919",
+    "url": "/pokedex/static/js/runtime-main.bc7fe0b4.js"
+  }
+]);
